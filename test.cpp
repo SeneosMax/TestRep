4b04b2d59af5d16c92717936280dbb3e11d16880
Maximilian <Maximilian.schaefer@seneos.com>
2015-08-18 09:05:55 +0200

    Added two test. The second should fail

--- test.cpp
+++ test.cpp
@@ -9,6 +9,15 @@ TEST(TestCaseCI1, TestTwo){
 	 ASSERT_EQ(1, toTest2(6,5));
 }
 
+TEST(TestCaseCI1, TestThree){
+	ASSERT_EQ(3,toTest(3));
+}
+
+TEST(TestCaseCI1, TestFour){
+	ASSERT_EQ(2, toTest2(6,4));
+	ASSERT_EQ(3, toTest2(3,4));
+}
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
